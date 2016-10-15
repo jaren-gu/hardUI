7ff100a7ebb33a07ead61f5de2f39e39cbe6f10a
jaren <13428282005@163.com>
2016-10-16 01:28:05 +0800

    修改 gulp 任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+'use strict'
+
 var gulp = require('gulp')
 var jshint = require('gulp-jshint')
 var sass = require('gulp-sass')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
+var autoprefixer = require('gulp-autoprefixer')
+var minicss = require('gulp-minify-css')
 
 /**
  *  check scripts
@@ -16,26 +20,32 @@ gulp.task('lint', function () {
 })
 
 /**
+ * compress scripts
+ * 合并脚本
+ */
+gulp.task('concatjs', function () {
+    gulp.src('./js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+})
+
+/**
  * compile sass
  *  编译 sass
  */
 gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
+    gulp.src('./scss/*.scss', { base: 'src' })
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
-})
-
-/**
- * compress scripts
- * 合并脚本
- */
-gulp.task('scripts', function () {
-    gulp.src('./js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(minicss())
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest('./dist/css'))
 })
 
 /**
@@ -43,10 +53,14 @@ gulp.task('scripts', function () {
  *  默认任务
  */
 gulp.task('default', function () {
-    gulp.run('lint', 'sass', 'scripts')
+    gulp.run('lint', 'concatjs', 'sass')
 
     // watching the files
     gulp.watch('./js/*.js', function () {
-        gulp.run('lint', 'sass', 'scripts')
+        gulp.run('lint', 'concatjs')
+    })
+
+    gulp.watch('./scss/*.scss', function () {
+        gulp.run('sass')
     })
 })
